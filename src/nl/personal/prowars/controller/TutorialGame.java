3f3ee393e174066475bec945d942fb34dca0eb89
Gorgony <n.j.vannispen@gmail.com>
2014-12-29 20:21:33 +0100

    images references updated

--- src/nl/personal/prowars/controller/TutorialGame.java
+++ src/nl/personal/prowars/controller/TutorialGame.java
@@ -45,8 +45,8 @@ public class TutorialGame extends BasicGame {
 
     @Override
     public void init(GameContainer arg0) throws SlickException {
-        left = new Image("images/Left.png");
-        circle = new Image("images/Block.png");
+        left = new Image("res/Left.png");
+        circle = new Image("res/Block.png");
 
     }
 
