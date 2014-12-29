4800c14c75273518584a8fb2c50719c8f9f3f150
Gorgony <n.j.vannispen@gmail.com>
2014-12-29 20:11:44 +0100

    tutorialgame

--- src/nl/personal/prowars/controller/main.java
+++ src/nl/personal/prowars/controller/main.java
@@ -4,4 +4,7 @@ package nl.personal.prowars.controller;
  * Created by Nathan on 29/12/2014.
  */
 public class main {
+    public static void main(String[] args)  {
+        System.out.println("test");
+    }
 }
